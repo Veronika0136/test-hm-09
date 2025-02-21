397d89b8f9e039c082bbeb8dec9334e7f7c125fa
Veronika0136 <Artemova331@gmail.com>
2025-02-21 23:54:37 +0200

    Create form.js

---
+++ js/form.js
@@ -0,0 +1,53 @@
+const formData = {
+  email: '',
+  message: '',
+};
+
+const stKey = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+function saveToLS(key, value) {
+  const jsonData = JSON.stringify(value);
+  localStorage.setItem(key, jsonData);
+}
+
+function loadFromLS(key) {
+  const body = localStorage.getItem(key);
+  try {
+    const data = JSON.parse(body);
+    return data;
+  } catch {
+    return body;
+  }
+}
+
+refs.form.addEventListener('input', e => {
+  const email = e.currentTarget.elements.email.value;
+  const message = e.currentTarget.elements.message.value;
+  formData.email = email;
+  formData.message = message;
+  saveToLS(stKey, formData);
+});
+
+function initPage() {
+  const data = loadFromLS(stKey);
+  refs.form.elements.email.value = data?.email || '';
+  refs.form.elements.message.value = data?.message || '';
+}
+
+initPage();
+
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+  } else {
+    console.log(formData);
+    localStorage.removeItem(stKey);
+    e.target.reset();
+  }
+});
